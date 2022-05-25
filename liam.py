b60f59f2aa6495f83a9e72f8faf18c820b3b55b7
timhaller <67918170+timhaller@users.noreply.github.com>
2022-05-25 21:08:24 +0200

    Add files via upload

---
+++ liam.py
@@ -0,0 +1,12 @@
+import keyboard
+import time
+
+keyboard.wait(' ')
+
+for i in range(20):
+
+    keyboard.write('@Rexar')
+    keyboard.press_and_release("enter")
+    keyboard.press_and_release("enter")
+    time.sleep(0.5)
+
